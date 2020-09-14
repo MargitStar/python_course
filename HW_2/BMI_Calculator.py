e3a9c470b48dc47b59284c1fb97d18263a633546
MargitStar <sovenokbymargo@mail.ru>
2020-09-14 22:27:04 +0300

    Refactor

--- HW_2/BMI_Calculator.py
+++ HW_2/BMI_Calculator.py
@@ -6,8 +6,8 @@ print("Please, enter your height in meters!")
 height = float(input())
 
 bmi_index = weight / (height ** 2)
-
 print(f"Your BMI index is {bmi_index}")
+
 equal_signs_before = int((bmi_index - 10) / 2)
 equal_signs_after = int((50 - bmi_index) / 2 - 1)
 print(f"10{'='*equal_signs_before}|{'='*equal_signs_after}50")
