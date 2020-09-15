df9b9555779e0a055f098f98dda4b112d4b90669
MargitStar <sovenokbymargo@mail.ru>
2020-09-15 15:53:56 +0300

    Refactor

--- HW_2/BMI_Calculator.py
+++ HW_2/BMI_Calculator.py
@@ -8,7 +8,11 @@ height = float(input())
 bmi_index = weight / (height ** 2)
 print(f"Your BMI index is {bmi_index}")
 
-equal_signs_before = int((bmi_index - 10) / 2)
-equal_signs_after = int((50 - bmi_index) / 2 - 1)
-print(f"10{'='*equal_signs_before}|{'='*equal_signs_after}50")
+max_index = 50
+min_index = 10
+
+equal_signs_before = int((bmi_index - min_index) / 2)
+equal_signs_after = int((max_index - bmi_index) / 2 - 1)
+print(f"{min_index}{'='*equal_signs_before}|{'='*equal_signs_after}{max_index}")
 print(' ' * (equal_signs_before + 1), round(bmi_index))
+
