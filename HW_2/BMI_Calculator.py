59a0f2a32f7c99d9e8ddd6879af2446e05ddcde7
MargitStar <sovenokbymargo@mail.ru>
2020-09-15 22:07:49 +0300

    Refactor

--- HW_2/BMI_Calculator.py
+++ HW_2/BMI_Calculator.py
@@ -1,4 +1,6 @@
 """This calculator helps you count your body index"""
+max_index = 50
+min_index = 10
 
 print("Please, enter your weight in kilos!")
 weight = float(input())
@@ -7,9 +9,6 @@ height = float(input())
 
 bmi_index = weight / (height ** 2)
 print(f"Your BMI index is {bmi_index}")
-
-max_index = 50
-min_index = 10
 
 equal_signs_before = int((bmi_index - min_index) / 2)
 equal_signs_after = int((max_index - bmi_index) / 2 - 1)
