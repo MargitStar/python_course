4706ddbee09b2c7b494a309fd8d7f0e6eff54a4a
MargitStar <sovenokbymargo@mail.ru>
2020-09-16 22:59:35 +0300

    Add folder HW_3

---
+++ HW_3/ex_1.py
@@ -0,0 +1,15 @@
+amount_of_numbers = 0
+number_list = []
+
+while amount_of_numbers < 3:
+    number_list.append(int(input()))
+    amount_of_numbers += 1
+
+if number_list[0] > 50 and (number_list[1] > number_list[0]\
+     or number_list[2] > number_list[0]):
+     print("Вася")
+    
+if number_list[0] == 5 or number_list[1] == number_list[2] == 7:
+    print("Петя")
+
+
