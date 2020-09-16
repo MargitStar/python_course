7245d625d000dc1ef44bf14e9662db6dd80b3fe3
MargitStar <sovenokbymargo@mail.ru>
2020-09-16 23:11:53 +0300

    Add 3, 4 tasks

--- HW_3/ex_1.py
+++ HW_3/ex_1.py
@@ -5,6 +5,11 @@ while amount_of_numbers < 3:
     number_list.append(int(input()))
     amount_of_numbers += 1
 
+if number_list[0] > number_list[1] + number_list[2]:
+    print(number_list[0] - number_list[1] - number_list[2])
+elif number_list[0] <= number_list[1] + number_list[2]:
+    print(number_list[1] + number_list[2] - number_list[0])
+
 if number_list[0] > 50 and (number_list[1] > number_list[0]\
      or number_list[2] > number_list[0]):
      print("Вася")
