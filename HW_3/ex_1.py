83c1a8342e662584f1c2eab7e764efa40c2686ad
MargitStar <sovenokbymargo@mail.ru>
2020-09-17 17:36:06 +0300

    Add 1 task

--- HW_3/ex_1.py
+++ HW_3/ex_1.py
@@ -2,47 +2,31 @@ number_list = []
 
 while len(number_list) < 3:
     number_list.append(int(input()))
+print()
 
-zero_count = number_list.count(0)
+print("#1")
+result = number_list[0] and number_list[1] and number_list[2] and "Нет нулевых значений"
+print(result,"\n")
 
-result = number_list[0] and number_list[1] and number_list[2]\
-     or "Нет нулевых значений"
-print(result)
-
+print("#2")
 result1 = number_list[0] or number_list[1] or number_list[2] or "Введены все нули"
-print("Result 1",result1)
+print(result1,"\n")
 
-result = (number_list[0] == 0 and number_list[1] == 0 and\
- number_list[2] == 0) or "Нет нулевых значений"
-
-
-
-# while zero_count == 0:
-#     print("Нет нулевых значений!")
-#     break
-#
-# while zero_count > 0:
-#     print(0)
-#     break
-
-# while zero_count == 3:
-#     print("Введены все нули!")
-#     break
-
-# for i in range(len(number_list)):
-#     number_list.
-
-
+print("#34")
 if number_list[0] > number_list[1] + number_list[2]:
-    print(number_list[0] - number_list[1] - number_list[2])
+    print(number_list[0] - number_list[1] - number_list[2],"\n")
 elif number_list[0] <= number_list[1] + number_list[2]:
-    print(number_list[1] + number_list[2] - number_list[0])
+    print(number_list[1] + number_list[2] - number_list[0],"\n")
+
 
 if number_list[0] > 50 and (number_list[1] > number_list[0]\
      or number_list[2] > number_list[0]):
-     print("Вася")
+     print("#5")
+     print("Вася\n")
+
     
 if number_list[0] == 5 or number_list[1] == number_list[2] == 7:
+    print("#6")
     print("Петя")
 
 
