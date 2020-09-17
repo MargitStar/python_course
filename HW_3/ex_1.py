2e2ed294eb0eca1913aff8c60276cb61669bed5f
MargitStar <sovenokbymargo@mail.ru>
2020-09-17 17:03:39 +0300

    Add 2 task

--- HW_3/ex_1.py
+++ HW_3/ex_1.py
@@ -1,9 +1,37 @@
-amount_of_numbers = 0
 number_list = []
 
-while amount_of_numbers < 3:
+while len(number_list) < 3:
     number_list.append(int(input()))
-    amount_of_numbers += 1
+
+zero_count = number_list.count(0)
+
+result = number_list[0] and number_list[1] and number_list[2]\
+     or "Нет нулевых значений"
+print(result)
+
+result1 = number_list[0] or number_list[1] or number_list[2] or "Введены все нули"
+print("Result 1",result1)
+
+result = (number_list[0] == 0 and number_list[1] == 0 and\
+ number_list[2] == 0) or "Нет нулевых значений"
+
+
+
+# while zero_count == 0:
+#     print("Нет нулевых значений!")
+#     break
+#
+# while zero_count > 0:
+#     print(0)
+#     break
+
+# while zero_count == 3:
+#     print("Введены все нули!")
+#     break
+
+# for i in range(len(number_list)):
+#     number_list.
+
 
 if number_list[0] > number_list[1] + number_list[2]:
     print(number_list[0] - number_list[1] - number_list[2])
