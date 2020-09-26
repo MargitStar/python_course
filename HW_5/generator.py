81b0a3a488306f72ccb9827bff75058a6768c9d9
MargitStar <sovenokbymargo@mail.ru>
2020-09-26 21:15:51 +0300

    Refactor

--- HW_5/generator.py
+++ HW_5/generator.py
@@ -1,11 +1,10 @@
 def my_generator(start):
-    while start != 100:
+    while start < 100:
         if start % 3 == 0:
             yield "Василий"
-            start += 1
         else:
             yield start
-            start += 1
+        start += 1
 
 
 generator = my_generator(1)
